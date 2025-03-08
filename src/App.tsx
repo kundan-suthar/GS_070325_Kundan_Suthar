85cac16f289944f1731d5bce4bf58e81113f2790
Kundan <kundanss22@gmail.com>
2025-03-08 11:59:32 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1>Welcome to Gsynergy Dashboard.</h1>
+      <Button>Click me</Button>
     </>
   );
 }
